d8a61ae2df6afea52e16d731f7744fe472d5e80e
oyzy <670882045@qq.com>
2017-07-06 14:52:37 +0800

    建造者模式

--- src/designPatterns/builder/Test.java
+++ src/designPatterns/builder/Test.java
@@ -4,9 +4,10 @@ package designPatterns.builder;
  * Created by Administrator on 2017/7/6.
  */
 public class Test {
+
     public static void main(String[] args) {
         Builder builder = new Builder();
         builder.produceMailSender(10);
-        builder.produceSmsSender(1);
+
     }
 }
