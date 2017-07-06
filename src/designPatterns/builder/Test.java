05735b8971cc8c95e6e9e2938d334e3c5c0a2b17
oyzy <670882045@qq.com>
2017-07-06 11:51:18 +0800

    建造者模式

--- src/designPatterns/builder/Test.java
+++ src/designPatterns/builder/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         Builder builder = new Builder();
         builder.produceMailSender(10);
+        builder.produceSmsSender(1);
     }
 }
