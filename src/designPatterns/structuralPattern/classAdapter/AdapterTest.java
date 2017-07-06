70c1aabd5c0bc7f967e14418a035bbca99cbfd95
oyzy <670882045@qq.com>
2017-07-06 15:42:03 +0800

    类的适配器模式

---
+++ src/designPatterns/structuralPattern/classAdapter/AdapterTest.java
@@ -0,0 +1,12 @@
+package designPatterns.structuralPattern.classAdapter;
+
+/**
+ * Created by Administrator on 2017/7/6.
+ */
+public class AdapterTest {
+    public static void main(String[] args) {
+        Targetable targetable = new Adapter();
+        targetable.method1();
+        targetable.method2();
+    }
+}
